02e7f3dcc9abde9b7a0dcb3d9a7c48cf83383185
Miikka Saxlund <miikka.saxlund@outlook.com>
2020-10-08 23:30:33 +0300

    Update luiscamachoc-palindromeNumber-csharp.cs
    Added the required comment

--- problems/luiscamachoc-palindromeNumber-csharp.cs
+++ problems/luiscamachoc-palindromeNumber-csharp.cs
@@ -1,3 +1,10 @@
+/**
+ * Solution to Palindrome Number problem at LeetCode in C++
+ *
+ * author: luiscamachoc
+ * ref: https://leetcode.com/problems/palindrome-number/
+ */
+
 public class Solution {
     public bool IsPalindrome(int x) {
          int reverseNumber = 0;
