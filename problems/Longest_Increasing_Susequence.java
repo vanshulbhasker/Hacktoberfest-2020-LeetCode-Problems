02fdfe7eb2ae758b811723b9316ced5151433e90
AnkitaM84 <60255963+AnkitaM84@users.noreply.github.com>
2020-10-09 17:19:57 +0530

    Create Longest_Increasing_Susequence.java
    Leetcode solution for LIS
    Time complexity is n^2

---
+++ problems/Longest_Increasing_Susequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int m = 1;
+        if(n == 0) {
+            return 0;
+        }
+        int dp[] = new int[n];
+        dp[0] = 1;
+        for(int i = 1; i < n; i++) {
+            int max = -1;
+            for(int j = 0; j < i; j++) {
+                if(nums[j] < nums[i] && dp[j] > max) {
+                    max = dp[j];
+                }
+            }
+            dp[i] = Math.max(1, max+1);
+            m = Math.max(m, dp[i]);
+        }
+        return m;
+    }
+}
